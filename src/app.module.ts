1668a0ea3d65a0ca03e1b8f3b737c829ad52ff07
Jose Junior <juniorferreira1919@hotmail.com>
2026-04-18 13:27:03 -0300

    chore: delaying development

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationModule } from '@shared/infra/configurations/configuration.module';
-import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [ConfigurationModule, UserModule],
+  imports: [ConfigurationModule],
   controllers: [],
   providers: [],
 })
